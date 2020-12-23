04aadd92d451e60deadd2f699b9e22d8fe3ab228
Jordan Kondracki <jordankondracki1999@gmail.com>
2020-12-23 13:18:36 +0100

    changed welcome message to image

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
 import discord
+import discord.utils
 from discord.ext import commands
 from datetime import date
+import requests
+from PIL import Image, ImageDraw
+from io import BytesIO
 
 TOKEN = ''
 
@@ -8,10 +12,46 @@ intent = discord.Intents.default()
 intent.members = True
 client = discord.Client(intents=intent)
 
+# def get_nsfw():
+#    response = requests.get
+#    ("https://scrolller.com/r/nsfw")
+#    json_data = json.loads(response.)
+
+
+def welcome_mess(avatar):
+    response = requests.get(avatar)
+    img = Image.open(BytesIO(response.content))
+    img.thumbnail((435, 435), Image.ANTIALIAS)
+    with Image.open('C:\\Users\\JordanK\\Desktop\\FAKER-BOT\\background2.png', 'r') as background:
+        with Image.new("L", img.size, 0) as mask:
+            img_w, img_h = img.size
+            bg_w, bg_h = background.size
+            offset = ((bg_w - img_w) // 2 + 8, (bg_h - img_h) - 214)
+            mask_draw = ImageDraw.Draw(mask)
+            mask_draw.ellipse([(0, 0), img.size], fill=255)
+            background.paste(img, offset, mask=mask)
+    background.resize((400, 195))
+    background.save('welcome.png')
+
+
+def welcome_mess2(avatar):
+    response = requests.get(avatar)
+    img = Image.open(BytesIO(response.content))
+    img.thumbnail((428, 428), Image.ANTIALIAS)
+    with Image.open('C:\\Users\\JordanK\\Desktop\\FAKER-BOT\\background1.png', 'r') as background:
+        with Image.new("L", img.size, 0) as mask:
+            img_w, img_h = img.size
+            bg_w, bg_h = background.size
+            offset = ((bg_w - img_w) // 2, (bg_h - img_h) - 316)
+            mask_draw = ImageDraw.Draw(mask)
+            mask_draw.ellipse([(0, 0), img.size], fill=255)
+            background.paste(img, offset, mask=mask)
+    background.resize((400, 195))
+    background.save('welcome.png')
+
 
 @client.event
 async def on_message(message):
-    # we do not want the bot to reply to itself
     if message.author == client.user:
         return
 
@@ -33,15 +73,34 @@ async def on_message(message):
         with open('image.jpg', 'rb') as image:
             await message.channel.send(file=discord.File(image, 'image.jpg'))
 
+    if message.content.startswith('!msgId'):
+        msg = '<@214056552217182209>\n' \
+              'r u c h a n i e\n' \
+              'sex'
+        await message.channel.send(msg)
+
+    if message.content.startswith('!halo'):
+        author = message.author
+        await message.channel.send(author.avatar_url)
+
+    if message.content.startswith('!test'):
+        channel = client.get_channel(790897876192591923)
+        author = message.author
+        welcome_mess(author.avatar_url)
+        with open('welcome.png', 'rb') as welcome:
+            await message.channel.send(file=discord.File(filename='welcome.png', fp=welcome))
+
 
 @client.event
 async def on_member_join(member):
     channel = client.get_channel(790897876192591923)
-    msg = f"{member.mention} jesteś {member.guild.member_count} użytkownikiem naszego serwera!" \
+    msg = f"{member.mention} jesteś {member.guild.member_count} użytkownikiem naszego serwera! {member.avatar_url}" \
           f"\n" \
           f"\n" \
-          f"{date.today().strftime('%d/%m/%Y')}"
-    embed = discord.Embed(title='Hello!', description=msg, color=0x00ff00)
+          f"{date.today().strftime('%d.%m.%Y')}"
+    embed = discord.Embed(title='Hello!', description=msg, color=0xfdf800)
+    embed.set_thumbnail(url=member.avatar_url)
+    await member.add_roles(discord.utils.get(member.guild.roles, name='Essa'))
     await channel.send(embed=embed)
 
 
